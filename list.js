8ae11be743af09d9380b7b8294fdae00fb659e06
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-13 23:59:17 +0100

    add function to remove paragraph

--- list.js
+++ list.js
@@ -2,13 +2,16 @@ let inputField = document.querySelector('#inputField');
 let addToDoButton = document.querySelector('#addToDo');
 let toDoContainer = document.querySelector('#toDoContainer');
 
-addToDoButton.addEventListener('click', function(){
+addToDoButton.addEventListener('click', function () {
     const paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
     inputField.value = "";
-    paragraph.addEventListener('click',function(){
-    paragraph.style.textDecoration = 'line-through'
+    paragraph.addEventListener('click', function () {
+        paragraph.style.textDecoration = 'line-through';
+    })
+    paragraph.addEventListener('dblclick', function () {
+        toDoContainer.removeChild(paragraph);
     })
 })
